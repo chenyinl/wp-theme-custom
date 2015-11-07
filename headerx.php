0b6924d883e19f0a6b22ea2188ed4282465ad7ff
CLIN <linchenyin@gmail.com>
2015-11-06 19:15:10 -0800

    First Commit

---
+++ headerx.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+    <style>
+.toolbar {
+    background: #1f4a8a;
+    #line-height: 38px;
+    height: 38px;
+    width: 100%;
+    overflow: hidden;
+   # font-size: 1.4em;
+    
+    
+}
+
+li {
+    list-style: none;
+    text-align: -webkit-match-parent;
+    display: inline;
+    color: #fff !important;
+    float: right;
+    margin: 0 0 0 0;
+}
+
+.rightmenu{
+    color: #fff !important;
+    font-size: 14px;
+    vertical-align:middle;
+}
+.toolbar .rightmenu ul li a.myaccount { 
+    background: url(http://cloudurl.com/templates/universe/images/icon-myaccount.png) left center no-repeat; 
+    padding: 0 15px 0 15px; 
+}
+.toolbar .rightmenu ul li a.contact { 
+    background: url(http://cloudurl.com/templates/universe/images/icon-contact.png) left center no-repeat; 
+    padding: 0 15px 0 21px; 
+}
+.toolbar .rightmenu ul li a.register { 
+    background: url(http://cloudurl.com/templates/universe/images/icon-register.png) left center no-repeat; 
+    padding: 0 15px 0 17px; 
+}
+#menu-clientarea{
+    margin: 8px 0 0 0;
+}
+
+nav li:hover, nav li.active { /* Hover background (gradients with single color fall-back) */
+    background: #0f9d58 url("../images/menuhover.png") top left repeat-x !important; /* Background color (repeated image by default) */
+
+}
+nav ul li a {
+    padding: 0 10px !important;
+}
+
+.img-logo{
+    max-width: 253px;
+    float: left;
+}
+.logo{
+    float: left;
+    z-index: 10000;
+}
+nav{
+    box-sizing: border-box;
+    display: block;
+    float: right;
+    font-family: MyriadProRegular, Arial, Helvetica, sans-serif, Calibri;
+    height: 38px;
+    line-height: 10px;
+    list-style-image: none;
+    list-style-position: outside;
+    list-style-type: none;
+    margin-bottom: 0px;
+    margin-left: 0px;
+    margin-right: 0px;
+    margin-top: 7px;
+    max-width: 1250px;
+    outline-color: rgb(0, 0, 0);
+    outline-style: none;
+    outline-width: 0px;
+    padding-bottom: 0px;
+    padding-left: 0px;
+    padding-right: 0px;
+    padding-top: 0px;
+
+}
+
+.mainmenu{
+    color: #000;
+}
+</style>
+</head>
+<script>
+document.addEventListener("DOMContentLoaded", function(event) { 
+    showz();
+    //your code to run since DOM is loaded and ready
+});
+function showz(){
+    alert(document.getElementById("post-1").style.zIndex);
+}
+</script>
+<body <?php body_class(); ?>>
+<!-- tool bar -->
+<div class="toolbar">
+    <div class="toolbarinner">
+		<div class="rightmenu">
+			<ul id="menu-clientarea">
+				<li><a href="clientarea.php" class="rightmenu myaccount">Client Area</a></li>
+				<li><a href="register.php" class="rightmenu register">Register</a></li>			        
+				<li><a href="contact.php" class="rightmenu contact">(866)560-9519 </a></li>          	
+		    </ul>
+        </div><!-- .rightmenu -->
+	</div><!-- .toolbarinner -->
+</div><!-- .toolbar -->
+<div id="page" class="hfeed site">
+    <!-- header -->
+    <header id="masthead" class="site-header" role="banner>
+    <div class="logo">
+		<a href="index.php">
+			<img src="http://cloudurl.com/templates/universe/images/logo.png" class="img-respond img-logo" alt="Cloud URL logo">
+		</a>
+	</div>
+    <div id="navbar" class="navbar">
+        <nav>
+            <ul>
+                <li><a class="mainmenu" href="portal.php">Help</a></li>
+                <li><a class="mainmenu" href="affiliate-program.php">Promote CloudURL</a></li>
+                <li><a class="mainmenu" href="domainchecker.php">Domains</a></li>
+                <li><a class="mainmenu" href="cart.php?a=add&amp;pid=4">WordPress Hosting</a></li>
+                <li><a class="mainmenu" href="web-hosting.php">Web Hosting</a></li>
+            </ul>
+        </nav>
+    </div>
+    </header>
+    <!-- endof header -->
+	<div id="main" class="site-main">
